2e300c2ce4489e81e5a216679479df99e860f591
Vishma Pratim Das <bhishmadas9@gmail.com>
2022-10-20 17:00:24 +0530

    Refactored the leftButton.setOnClickListener and rightButton.setOnClickListener into a showResult() function in the onCreate() lifecycle event

--- app/src/main/java/com/example/biggernumberapp/MainActivity.kt
+++ app/src/main/java/com/example/biggernumberapp/MainActivity.kt
@@ -15,47 +15,49 @@ class MainActivity : AppCompatActivity() {
 
         obj = ActivityMainBinding.inflate(layoutInflater)   // populate object
         setContentView(obj.root)    // set screen as object root
+        this.showResult()
+    }
 
+    private fun showResult() {
         obj.leftButton.setOnClickListener {
-            var num1 = obj.leftButton.text.toString().toInt()
-            var num2 = obj.rightButton.text.toString().toInt()
+            val num1 = obj.leftButton.text.toString().toInt()
+            val num2 = obj.rightButton.text.toString().toInt()
 
             if (num1 > num2) {
                 // SHOW TOAST
-                Toast.makeText(this,"You won",Toast.LENGTH_SHORT).show()
-                // CHANGE BACKGROUND COLOR
-                obj.BackgoundView.setBackgroundColor(Color.GREEN)
-            }
-            else {
-                // SHOW TOAST
-                Toast.makeText(this, "You suck bro! Try again!"+"TIP: PICK THE BIGGER NUMBER!",Toast.LENGTH_SHORT).show()
-                // CHANGE BACKGROUND COLOR
-                obj.BackgoundView.setBackgroundColor(Color.RED)
-            }
-            // CHANGE UNCLICKED NUMBER
-            // FOR THE SAKE OF REUSABILITY WE WILL USE A FUNCTION (YAY LESS CODE!)
-            changeNumbers()
-        }
-
-        obj.rightButton.setOnClickListener {
-            var num1 = obj.leftButton.text.toString().toInt()
-            var num2 = obj.rightButton.text.toString().toInt()
-
-            if (num1 < num2) {
-                // SHOW TOAST
-                Toast.makeText(this,"You won",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,"You won", Toast.LENGTH_SHORT).show()
                 // CHANGE BACKGROUND COLOR
                 obj.BackgoundView.setBackgroundColor(Color.GREEN)
             } else {
                 // SHOW TOAST
                 Toast.makeText(this, "You suck bro! Try again!"
-                    +"TIP: PICK THE BIGGER NUMBER!",Toast.LENGTH_SHORT).show()
+                        +"TIP: PICK THE BIGGER NUMBER!", Toast.LENGTH_SHORT).show()
+                // CHANGE BACKGROUND COLOR
+                obj.BackgoundView.setBackgroundColor(Color.RED)
+            }
+            changeNumbers()
+        }
+
+        obj.rightButton.setOnClickListener {
+            val num1 = obj.leftButton.text.toString().toInt()
+            val num2 = obj.rightButton.text.toString().toInt()
+
+            if (num1 < num2) {
+                // SHOW TOAST
+                Toast.makeText(this,"You won", Toast.LENGTH_SHORT).show()
+                // CHANGE BACKGROUND COLOR
+                obj.BackgoundView.setBackgroundColor(Color.GREEN)
+            } else {
+                // SHOW TOAST
+                Toast.makeText(this, "You suck bro! Try again!"
+                        +"TIP: PICK THE BIGGER NUMBER!", Toast.LENGTH_SHORT).show()
                 // CHANGE BACKGROUND COLOR
                 obj.BackgoundView.setBackgroundColor(Color.RED)
             }
             changeNumbers()
         }
     }
+
     private fun changeNumbers() {
         obj.leftButton.text = (0..100).random().toString()
         obj.rightButton.text = (0..100).random().toString()
